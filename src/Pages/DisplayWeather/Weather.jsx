2c70d47c72c0b429a70d18f3ae1b0bd77d2ffaca
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-05-02 23:23:39 +0600

    api object explain by objects

--- src/Pages/DisplayWeather/Weather.jsx
+++ src/Pages/DisplayWeather/Weather.jsx
@@ -6,6 +6,22 @@ const Weather = ({ temperature, locationName }) => {
     console.log(locationName, "weather component")
     const [WeatherData] = getWeatherData(locationName);
     console.log(WeatherData, "weather component")
+    const ApiObjectKeys = [
+        { "coord": "its for map integration(longitude and latitude)" },
+        { "weather": "It includes an ID, a description of the weather,main info(sky clear), and an icon code" },
+        { "base": "" },
+        { "main": "This object contains various temperature, pressure, sea level, ground level, max-min temperature, and humidity values related to the current weather conditions" },
+        { "visibility": "" },
+        { "wind": "" },
+        { "clouds": "" },
+        { "dt": "" },
+        { "sys": "" },
+        { "timezone": "" },
+        { "id": "Country Id" },
+        { "name": "City Name" },
+        { "code": "http status-code" }
+    ]
+
     return (
         <div>
             <div>
