1765633611f98dfdd332c2f0d66369521e018651
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-06-24 05:53:56 +0600

    info add

--- src/Pages/DisplayWeather/WeatherBody.jsx
+++ src/Pages/DisplayWeather/WeatherBody.jsx
@@ -5,6 +5,8 @@ import nightBg from "../../assets/images/Clear Night.webp"
 import Loader from '../../assets/Loader/Loader';
 import NotFound from '../../assets/NotFound/NotFound';
 import { toast } from 'react-toastify';
+import {BiInfoCircle} from 'react-icons/bi'
+import {IoInformationCircle} from 'react-icons/io5'
 
 
 const WeatherBody = ({ SelectedLocation,handleSelectChange }) => {
@@ -55,7 +57,7 @@ const WeatherBody = ({ SelectedLocation,handleSelectChange }) => {
                             <p>{data?.main?.temperature}</p>
                             <p className='my-6 md:my-3 text-lg'>{formattedTime}</p>
                             <div className='md:w-[500px] flex justify-start items-center'>
-                                <div className='flex items-center '>
+                                <div className='flex space-x-2 items-center justify-start -ml-2'>
                                     <img className='pt-1' src={iconUrl} alt="icon" srcset="" />
                                     <p className='text-xl font-bold'>{celsiusTemperature}°C</p>
                                 </div>
@@ -72,24 +74,27 @@ const WeatherBody = ({ SelectedLocation,handleSelectChange }) => {
                     {/* Footer */}
                     <div className='w-full md:w-[500px] flex justify-between items-start'>
                         <p className='flex flex-col gap-1'>
-                            <span>Wind</span>
-                            <span>{(data.wind.speed * 2.23694).toFixed(0)} mph</span>
+                            <span className='cursor-pointer'>Wind <sup><BiInfoCircle className='inline-block -ml-[2px] text-[10px] mb-1'/></sup></span>
+                            <span>{(data.wind.speed * 2.23694).toFixed(0)} mph</span> 
                         </p>
                         <p className='flex flex-col gap-1'>
-                            <span>Humidity</span>
+                            <span className='cursor-pointer'>Humidity <sup><BiInfoCircle className='inline-block -ml-[2px] text-[10px]'/></sup></span>
                             <span>{data.main.humidity}%</span>
                         </p>
                         <p className='flex flex-col gap-1'>
-                            <span>Visibility</span>
+                            <span className='cursor-pointer'>Visibility <sup><BiInfoCircle className='inline-block -ml-[2px] text-[10px]'/></sup></span>
                             <span>{data.visibility}</span>
                         </p>
                         <p className='flex flex-col gap-1'>
-                            <span>Pressure</span>
+                            <span className='cursor-pointer'>Pressure <sup><BiInfoCircle className='inline-block -ml-[2px] text-[10px]'/></sup></span>
                             <span>{data.main.pressure}</span>
                         </p>
                         <p className='flex flex-col gap-1'>
-                            <span> Cloud Covarage</span>
-                            <span>{data.clouds.all}%</span>
+                            <span className='cursor-pointer'> Cloud Covarage <sup><BiInfoCircle className='inline-block -ml-[2px] text-[10px]'/></sup></span>
+                            <span>{data.clouds.all}%
+                           
+                            </span>
+                            
                         </p>
                     </div>
                 </div>
